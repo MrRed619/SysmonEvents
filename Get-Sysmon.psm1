d2f251fe17a7555a6d7344039e72b001b501e9be
MrRed619 <98003321+MrRed619@users.noreply.github.com>
2024-03-07 10:20:39 -0800

    Add files via upload

---
+++ Get-Sysmon.psm1
@@ -0,0 +1,21 @@
+﻿function Get-SysmonEvent{
+    param (
+        $eventid,
+        $start,
+        $end
+    )
+    $filters = @{LogName = "Microsoft-Windows-Sysmon/Operational"}
+    
+    if ($eventid -ne $null) {
+        $filters.ID = $eventid
+    }
+    
+    if ($start -ne $null) {
+        $filters.StartTime = $start
+    }
+
+    if ($end -ne $null) {
+        $filters.EndTime = $end
+    }
+    Get-WinEvent -FilterHashtable $filters
+}
